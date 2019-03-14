c6869ef8929fad9094c94a3c3794bfae355e611f
MourabitElBachir <bachirMorocco2018>
2019-03-14 10:09:24 +0100

    FilesTest configuration data_test folder creation

--- src/main/scala/services/Files.scala
+++ src/main/scala/services/Files.scala
@@ -6,22 +6,15 @@ import java.nio.file.Paths
 
 object Files {
 
-  def filterFiles(matchFirst: List[String], matchLast: List[String]): File =>
+  def filterMatching(matchFirst: List[String], matchLast: List[String]): File =>
     Boolean = file => matchFirst.exists(file.getName.startsWith(_)) && matchLast.exists(file.getName.endsWith(_))
 
   def getListOfFiles(dir: File, matchFirst: List[String], matchLast: List[String]): Stream[File] = {
     if (dir.exists && dir.isDirectory) {
-      dir.listFiles.filter(_.isFile).toStream.filter(filterFiles(matchFirst, matchLast))
+      dir.listFiles.filter(_.isFile).toStream.filter(filterMatching(matchFirst, matchLast))
     } else {
       Stream.empty
     }
-  }
-
-  def deleteFile(fileTemp: File): Boolean = {
-    if (fileTemp.exists) {
-      return fileTemp.delete()
-    }
-    false // Throw an exception
   }
 
   def makeFile[A](file: File, stream: Stream[A]): Unit = {
@@ -40,10 +33,6 @@ object Files {
       .isDirectory
   }
 
-  //  def getShopsList(dir: File, matchStrings: List[String]): Stream[String] = {
-  //    dir.listFiles.filter(_.isFile).toStream.filter { file =>
-  //      matchStrings.exists(file.getName.startsWith(_))
-  //    }.map(file => RegExp.matchMagasinFromReference(file.getName))
-  //  }
+  def r : String => Int = s => s.length
 
 }
