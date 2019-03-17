8fa2df7803edaa2d60507ddb6577a1c0e7b10cd2
MourabitElBachir <bachirMorocco2018>
2019-03-17 18:37:43 +0100

    Tests : Modifications

---
+++ src/test/scala/models/TurnoversTest.scala
@@ -0,0 +1,31 @@
+package models
+
+import org.scalatest.FunSuite
+
+class TurnoversTest extends FunSuite {
+
+  test("Turnover compare method test") {
+
+    val expected = -1
+
+    val turnover1 = Turnover(5, 66)
+    val turnover2 = Turnover(6, 67)
+
+    val result = turnover1.compare(turnover2)
+
+    assert(turnover1 === turnover2)
+  }
+
+  test("Turnover toString method test") {
+
+    val expected = "5|66.5\n"
+
+    val turnover = Turnover(5, 66.5)
+
+
+    val result = turnover.toString
+
+    assert(expected === result)
+  }
+
+}
